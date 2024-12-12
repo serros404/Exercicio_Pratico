4cea38ea54c064daccac6552cc26371d977afff0
serros404 <serros404@gmail.com>
2024-12-12 18:40:56 -0300

    Exercício I

--- ExercicioPratico/ExercicioPratico/Program.cs
+++ ExercicioPratico/ExercicioPratico/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Carro chevrolet = new("Sedan", "Chevrolet", "Onix", 2016, 110);
+chevrolet.Acelerar(chevrolet.Marca);
+
+Console.WriteLine($"{chevrolet.Modelo} {chevrolet.Montadora} {chevrolet.Marca}" +
+$" {chevrolet.Ano} {chevrolet.Potencia} CV");
+
+
+
+Carro ford = new("SUV", "Ford", "EcoSport", 2018, 120);
+ford.Acelerar(ford.Marca);
+
+Console.WriteLine($"{ford.Modelo} {ford.Montadora} {ford.Marca}" +
+$" {ford.Ano} {ford.Potencia} CV");
+
+Console.ReadKey();
+
+Console.ReadKey();
+
+public class Carro
+{
+    public string? Modelo;
+    public string? Montadora;
+    public string? Marca;
+    public int Ano;
+    public int Potencia;
+
+    public Carro(string? Modelo, string? Montadora, string? Marca, int Ano, int Potencia)
+    {
+        this.Modelo = Modelo;
+        this.Montadora = Montadora;
+        this.Marca = Marca;
+        this.Ano = Ano;
+        this.Potencia = Potencia;
+    }
+
+    public void Acelerar(string marca)
+    {
+        Console.WriteLine("\nAcelerando" + marca);
+    }
+}
+
+
