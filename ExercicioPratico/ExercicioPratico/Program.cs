57716c4ee6c287b6b18baef64b4f7c2f2fdd6753
serros404 <serros404@gmail.com>
2024-12-17 19:08:15 -0300

    Exercício III

--- ExercicioPratico/ExercicioPratico/Program.cs
+++ ExercicioPratico/ExercicioPratico/Program.cs
@@ -1,8 +1,13 @@
 ﻿Carro chevrolet = new("Sedan", "Chevrolet", "Onix", 2016, 110);
 chevrolet.Acelerar(chevrolet.Marca);
 
+
 Console.WriteLine($"{chevrolet.Modelo} {chevrolet.Montadora} {chevrolet.Marca}" +
 $" {chevrolet.Ano} {chevrolet.Potencia} CV");
+Console.WriteLine("Aumentando a Potencia (por valor : + 3CV) : ");
+var novaPotenciaChevrolet = chevrolet.AumentarPotencia(chevrolet.Potencia);
+Console.Write("-> " + novaPotenciaChevrolet);
+Console.WriteLine("\nO valor do campo Potencia não foi alterado : " + chevrolet.Potencia);
 
 
 Carro ford = new("SUV", "Ford", "EcoSport", 2018, 120);
@@ -10,6 +15,9 @@ ford.Acelerar(ford.Marca);
 
 Console.WriteLine($"{ford.Modelo} {ford.Montadora} {ford.Marca}" +
 $" {ford.Ano} {ford.Potencia} CV");
+var novaPotenciaFord = ford.AumentarPotencia(ref ford.Potencia);
+Console.Write("-> " + novaPotenciaFord);
+Console.WriteLine("\nO valor do campo Potencia foi alterado : " + ford.Potencia);
 
 Console.ReadKey();
 
@@ -41,13 +49,24 @@ public class Carro
 
     public void Acelerar(string marca)
     {
-        Console.WriteLine("\nAcelerando" + marca);
+        Console.WriteLine("\nAcelerando o meu... " + marca);
     }
 
     public double VelocidadeMaxima(int potencia)
     {
         return potencia * 1.75;
     }
+
+    public int AumentarPotencia(int potencia)
+    {
+        return potencia += 3;
+    }
+
+    public int AumentarPotencia(ref int potencia)
+    {
+        return potencia += 5;
+    }
+
 }
 
 
